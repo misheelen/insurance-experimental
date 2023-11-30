240156384fb8ac00a9c4ff042d564a05b53d2fdb
misheelen <misheel@Misheels-MacBook-Air.local>
2023-12-01 00:36:58 +0900

    converted body to carousel

--- server.js
+++ server.js
@@ -8,5 +8,5 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
